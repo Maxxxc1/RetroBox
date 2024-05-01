a7b07b75f31469dc99c44822a4af612873eebdbe
Doug Scheible <doug_scheible@yahoo.com>
2024-04-30 19:14:21 -0500

    added basic Game Model

---
+++ models/Game.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Game extends Model {}
+
+Game.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    release_date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    img_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    console: { 
+        type: DataTypes.STRING,
+        allowNull: true,
+        default: "Nintendo",
+    },
+    developer: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'game',
+  }
+);
+
+module.exports = Game;
