9b0bb0e6432ed72a5c33def8265318fbdc96a84c
Doug Scheible <doug_scheible@yahoo.com>
2024-05-05 19:22:35 -0500

    profile handlebar now rendered

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,12 +18,13 @@ router.get('/', async (req, res) => {
   }
 });
 
-/*router.get('/profile', withAuth, async (req, res) => {
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      //include: [{ model: Project }],
     });
 
     const user = userData.get({ plain: true });
@@ -60,26 +61,6 @@ router.get('/', async (req, res) => {
 //     res.status(500).json(err);
 //   }
 // });
-
-// // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Blog }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });*/
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
